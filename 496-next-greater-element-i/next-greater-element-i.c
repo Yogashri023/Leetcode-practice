7a0585a5bcda31bfb943321429b245fcc55d29d4
Yogashri023 <yogashrirr23@gmail.com>
2026-09-20 20:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (94.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.c
+++ 496-next-greater-element-i/next-greater-element-i.c
@@ -5,25 +5,24 @@ int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, in
     int* ans=(int*)malloc(nums1Size*sizeof(int));
     *returnSize=nums1Size;
 
-    for (int i=0;i<nums1Size;i++){
-        int v=nums1[i];
+    for(int i=0;i<nums1Size;i++){
+        int val=nums1[i];
         int in=-1;
-        for (int j=0;j<nums2Size;j++){
-            if (nums2[j]==v){
-                 in=j;
+        for(int j=0;j<nums2Size;j++){
+            if (nums2[j]==val){
+                in=j;
                 break;
             }
         }
-        int great=-1;
-        for (int k=in+1;k<nums2Size;k++){
-            if (nums2[k]>v){
-                        great=nums2[k];
-                        break;
-                        }
+        int g=-1;
+        for(int k=in+1;k<nums2Size;k++){
+            if(nums2[k]>val){
+                g=nums2[k];
+                break;
+            }
         }
-                   
-        ans[i]=great;
-                }
-            
-        return ans;
+        ans[i]=g;
     }
+    return ans;
+
+}
